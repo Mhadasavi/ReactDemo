afa7af9690f9b8dd894d7301437076c3c61d1ca9
Upkar <Upkar@DESKTOP-22L0J3T>
2022-12-20 21:49:18 +0530

    Use constant instead of hard code data in html

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,15 @@
 import "./ExpenseItem.css";
 function ExpenseItem() {
+  const expenseDate=new Date(2022,1,1);
+  const expenseDescription='Boat headphone';
+  const expenseItemprice=1800;
   return (
     <div className="expense-item">
-      <div>Date</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2> Description</h2>
+        <h2> {expenseDescription}</h2>
       </div>
-      <div className="expense-item__price">Price</div>
+      <div className="expense-item__price">{expenseItemprice}</div>
     </div>
   );
 }
