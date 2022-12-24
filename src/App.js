3a05935bcf140c1af83694c1f2752ef5900a6fb6
Upkar Chaudhary <hexaview9@whiteoakuk.com>
2022-12-24 21:27:10 +0530

    Convert date to calender format

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
   const expenseItems = [
-    { id: 1, itemName: "PC", itemPrice: 100, date: new Date(2022, 6, 25) },
-    { id: 2, itemName: "Mobile", itemPrice: 50, date: new Date(2022, 6, 25) },
+    { id: 1, itemName: "PC", itemPrice: 100, date: new Date(2022, 5, 25) },
+    { id: 2, itemName: "Mobile", itemPrice: 50, date: new Date(2022, 5, 25) },
   ];
   return (
     <div className="App">
